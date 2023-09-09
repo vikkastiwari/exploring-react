df7fb7bd04cc806d41086c5648f84bb90a38591f
vikas.t <vikas.t@pepperfry.com>
2023-09-09 12:58:52 +0530

    form data handling moved from useState to router provided hooks

--- react-poster/FE/src/components/PostsList/PostsList.js
+++ react-poster/FE/src/components/PostsList/PostsList.js
@@ -1,12 +1,16 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
 
 import classes from "./PostsList.module.css";
 import Post from "../Post/Post";
 
 function PostsList() {
-  const [posts, setPosts] = useState([]);
-  const [isFetching, setIsFetching] = useState(false);
-  const [isError, setIsError] = useState(false);
+  // now using LoaderData hook instead of useState
+  const posts =  useLoaderData();
+
+  // const [posts, setPosts] = useState([]);
+  // const [isFetching, setIsFetching] = useState(false);
+  // const [isError, setIsError] = useState(false);
 
   // calling fetch here will re-render the dom as data is updated
   // when it rerenders it will reload and it will again call fetch
@@ -19,78 +23,77 @@ function PostsList() {
 
   // Its preventing infinite loop from occuring
   // It doesn't take a function that returns a promise instead it deals with void or say cleanup code
-  
   /**
    * when does this execute ?
    * It executes based on second argument 
    * second argument can be dependency like function or variable so whenever it will change this useEffect will trigger
    * As we passed [] so it only executes once as it doesn't have any dependencies
    * */ 
-  useEffect(() => {
-    async function fetchPosts(){
-      setIsFetching(true);
-      let response;
-      try{
-        response = await fetch('http://localhost:8080/posts');
-      }catch(error) {
-        setIsError(true);
-        console.error('There was an error', error);
-      }
-      if(response?.ok){
-        const resData = await response.json();
-        setPosts(resData.posts);
-        setIsFetching(false);
-      }
-    }
-    fetchPosts();
-  },[]);
+  // useEffect(() => {
+  //   async function fetchPosts(){
+  //     setIsFetching(true);
+  //     let response;
+  //     try{
+  //       response = await fetch('http://localhost:8080/posts');
+  //     }catch(error) {
+  //       setIsError(true);
+  //       console.error('There was an error', error);
+  //     }
+  //     if(response?.ok){
+  //       const resData = await response.json();
+  //       setPosts(resData.posts);
+  //       setIsFetching(false);
+  //     }
+  //   }
+  //   fetchPosts();
+  // },[]);
 
-  function addPostHandler(postData) {
-    let response;
-    try{
-      response = fetch('http://localhost:8080/posts', {
-        method:'POST',
-        body: JSON.stringify(postData),
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      });
-    }catch(error)  {
-      setIsError(true);
-      console.error('There was an error', error);
-    }
-    if(response?.ok){
-      // technically the best way to use callback
-      // when updation depends on previous state
-      setPosts((exisitingPost) => [postData, ...exisitingPost]);
-    }
-  }
+  // function addPostHandler(postData) {
+  //   let response;
+  //   try{
+  //     response = fetch('http://localhost:8080/posts', {
+  //       method:'POST',
+  //       body: JSON.stringify(postData),
+  //       headers:{
+  //         'Content-Type': 'application/json'
+  //       }
+  //     });
+  //   }catch(error)  {
+  //     setIsError(true);
+  //     console.error('There was an error', error);
+  //   }
+  //   if(response?.ok){
+  //     // technically the best way to use callback
+  //     // when updation depends on previous state
+  //     setPosts((exisitingPost) => [postData, ...exisitingPost]);
+  //   }
+  // }
   
   return (
     <>
-      {!isError && !isFetching && posts.length && ( 
+      { posts.length && ( 
         <ul className={classes.posts}>
           {posts.map((post, i) => (
             <Post key={i} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
-      {!isError && !isFetching && !posts.length && (
+      { !posts.length && (
         <div style={{textAlign:'center', color:'white'}}>
           <h2>There are no posts added.</h2>
           <p>Start adding one!!</p>
         </div>
       )}
-      {!isError && isFetching && (
+      {/* {(
         <div style={{textAlign:'center', color:'white'}}>
           <p>Loading...</p>
         </div>
-      )}
-      {isError && !isFetching && (
+      )} */}
+      {/* {(
         <div style={{textAlign:'center', color:'red'}}>
           <p>Something went wrong...</p>
         </div>
-      )}
+      )} */}
     </>
   );
 }
--- react-poster/FE/src/routes/NewPost/NewPost.js
+++ react-poster/FE/src/routes/NewPost/NewPost.js
@@ -1,50 +1,72 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
+// import { useState } from "react";
+import { Form, Link, redirect } from "react-router-dom";
 
 import Modal from "../../components/Modal/Modal";
 import classes from './NewPost.module.css';
 
 function NewPost({onAddPost, onClose}) {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [enteredNameValue, setEnteredNameValue] = useState("");
+  // const [enteredValue, setEnteredValue] = useState("");
+  // const [enteredNameValue, setEnteredNameValue] = useState("");
 
-  function changeBodyHandler(event) {
-    setEnteredValue(event.target.value);
-  }
+  // function changeBodyHandler(event) {
+  //   setEnteredValue(event.target.value);
+  // }
 
-  function changeNameHandler(event) {
-    setEnteredNameValue(event.target.value);
-  }
+  // function changeNameHandler(event) {
+  //   setEnteredNameValue(event.target.value);
+  // }
 
-  function submitHandler(event) {
+  // function submitHandler(event) {
     // prevents browser from generating and sending http request
-    event.preventDefault();
-    const postData = {
-      body: enteredValue,
-      author: enteredNameValue
-    };
-    onAddPost(postData);
-    onClose();
-  }
+    // event.preventDefault();
+
+    // const postData = {
+    //   body: enteredValue,
+    //   author: enteredNameValue
+    // };
+    
+    // onAddPost(postData);
+    // onClose();
+  // }
 
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+     <Form method="post" className={classes.form}>  {/* onSubmit={submitHandler} */}
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+          <textarea id="body" name="body" required rows={3} /> {/*  onChange={changeBodyHandler}  */}
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input type="text" id="name" required onChange={changeNameHandler} />
+          <input type="text" id="name" name="author" required /> {/* onChange={changeNameHandler}  */}
         </p>
         <p className={classes.actions}>
           <Link type='button' to='/'>Cancel</Link>
           <button type='submit'>Submit</button>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+// It receives data from the router module automatically
+export async function action({request}){
+  const formData = await request.formData();
+  const postData  = Object.fromEntries(formData); // returns {body:'', author:''}
+  try{
+    await fetch('http://localhost:8080/posts', {
+      method:'POST',
+      body: JSON.stringify(postData),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    });
+  }catch(error)  {
+    console.error('There was an error', error);
+  }
+
+  // It returns the result of the calling function
+  return redirect('/');
+}
