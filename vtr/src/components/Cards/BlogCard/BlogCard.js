aa108386e4cd25269525f3e649a0a8da1e137db1
vikas.t <vikas.t@pepperfry.com>
2023-11-19 21:32:24 +0530

    blog card component created

---
+++ vtr/src/components/Cards/BlogCard/BlogCard.js
@@ -0,0 +1,37 @@
+import './BlogCard.css';
+import thumb_42x29 from '../../../assets/img/thumb/42-29.jpg';
+
+const Blog = (props) => {
+
+    return (
+        <div className="vtr_blog_card_wrapper">
+            <div className="image">
+                <a href={props.item.url}  target="_blank" rel="noopener noreferrer">
+                <img src={thumb_42x29} alt="thumb" />
+                <div
+                    className="main"
+                    style={{backgroundImage:`url(${require(`../../../assets/img/blog/${props.item.imgSrc}`)})`}}
+                ></div>
+                <div className="date">
+                    <h3>&#35;{props.item.rank}</h3>
+                </div>
+                </a>
+            </div>
+            <div className="details vtr_whitespace_normal">
+                {props.item.tags.map((tag,index)=>(
+                    <span key={index} className="category">
+                        <>&#35;{tag}&nbsp;</>
+                    </span>
+                ))}   
+                <a href={props.item.url} target="_blank" rel="noopener noreferrer">
+                    <h3 className="title">
+                        {props.item.title}
+                    </h3>
+                    <p>{props.item.desc}</p>
+                </a>
+            </div>
+        </div>
+    );
+};
+
+export default Blog;
--- vtr/src/sections/Blog/Blog.js
+++ vtr/src/sections/Blog/Blog.js
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom';
+
 import './Blog.css';
 import JsonData from '../../assets/data/content.json';
 import Section from "../../components/Section/Section";
-import thumb_42x29 from '../../assets/img/thumb/42-29.jpg';
-import { Link } from 'react-router-dom';
+import BlogCard from '../../components/Cards/BlogCard/BlogCard';
 
 const Blog = () => {
   const blogData = JsonData.blog;
@@ -24,34 +25,7 @@ const Blog = () => {
                   className="fadeInUp"
                   style={{visibility: "visible", animationDuration: '1s'}}
                 >
-                  <div className="list_inner">
-                    <div className="image">
-                      <a href={item.url}  target="_blank" rel="noopener noreferrer">
-                        <img src={thumb_42x29} alt="thumb" />
-                        <div
-                          className="main"
-                          data-img-url="img/blogs/1.jpg"
-                          style={{backgroundImage:`url(${require(`../../assets/img/blog/${item.imgSrc}`)})`}}
-                        ></div>
-                        <div className="date">
-                          <h3>&#35;{item.rank}</h3>
-                        </div>
-                      </a>
-                    </div>
-                    <div className="details vtr_whitespace_normal">
-                      {item.tags.map((tag,index)=>(
-                        <span key={index} className="category">
-                          <>&#35;{tag}&nbsp;</>
-                        </span>
-                      ))}   
-                      <a href={item.url} target="_blank" rel="noopener noreferrer">
-                        <h3 className="title">
-                            {item.title}
-                        </h3>
-                        <p>{item.desc}</p>
-                      </a>
-                    </div>
-                  </div>
+                  <BlogCard item={item} />
                 </li>
               ))}
             </ul>
